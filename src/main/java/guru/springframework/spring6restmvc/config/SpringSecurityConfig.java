c9b388382924dbb8cc4fc3ca9ad808ad4cc7684e
John <john.refaat88@gmail.com>
2025-02-07 01:52:57 +0200

    Adding Actuator

--- src/main/java/guru/springframework/spring6restmvc/config/SpringSecurityConfig.java
+++ src/main/java/guru/springframework/spring6restmvc/config/SpringSecurityConfig.java
@@ -1,8 +1,10 @@
 package guru.springframework.spring6restmvc.config;
 
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -14,13 +16,12 @@ import org.springframework.security.web.SecurityFilterChain;
 @Profile("!restassured")
 @Configuration
 public class SpringSecurityConfig {
+
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(authorizationManagerRequestMatcherRegistry ->
-                        authorizationManagerRequestMatcherRegistry
-                                //.anyRequest().permitAll())
-                                .requestMatchers("/v3/api-docs**", "/swagger-ui/index.html", "/swagger-ui.html").permitAll()
-                                    .anyRequest().authenticated())
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth ->
+                            auth.requestMatchers("/actuator/**", "/v3/api-docs**", "/swagger-ui/**",  "/swagger-ui.html").permitAll()
+                                .anyRequest().authenticated())
                 .oauth2ResourceServer(httpSecurityOAuth2ResourceServerConfigurer ->  {
                     httpSecurityOAuth2ResourceServerConfigurer.jwt(Customizer.withDefaults());
                 });
